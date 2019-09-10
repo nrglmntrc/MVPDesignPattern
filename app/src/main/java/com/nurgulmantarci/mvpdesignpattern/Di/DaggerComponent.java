e792f59e55fa85a9fa82fe9a17051701ac4cff6e
Nurgül Mantarcı <nurgulmantarci28@gmail.com>
2019-09-10 11:30:06 +0300

    Dagger İnjection

--- app/src/main/java/com/nurgulmantarci/mvpdesignpattern/Di/DaggerComponent.java
+++ app/src/main/java/com/nurgulmantarci/mvpdesignpattern/Di/DaggerComponent.java
@@ -1,4 +1,15 @@
 package com.nurgulmantarci.mvpdesignpattern.Di;
 
-public class DaggerComponent {
+import com.nurgulmantarci.mvpdesignpattern.Ui.CategoryListActivity.CategoryListActivity;
+import com.nurgulmantarci.mvpdesignpattern.Ui.CommandListActivity.CommandListActivity;
+
+import javax.inject.Singleton;
+
+import dagger.Component;
+
+@Singleton
+@Component(modules = {DaggerModule.class})
+public interface DaggerComponent {
+    void inject(CategoryListActivity categoryListActivity);
+    void inject(CommandListActivity commandListActivity);
 }
